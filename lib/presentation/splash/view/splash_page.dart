fd1eedbd6b4cb0d0ffea3cadcfbcbb051b93561b
WemersonDamasceno <wemersondamasceno7@gmail.com>
2023-12-27 10:40:27 -0300

    feat: create splash view

---
+++ lib/presentation/splash/view/splash_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:study_flow/presentation/splash/splash_imports.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const SplashView();
+  }
+}
