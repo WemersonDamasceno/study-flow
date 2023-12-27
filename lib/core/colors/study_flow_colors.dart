4dbbee30cb1ce337cec08c3fb090171f697dc51e
WemersonDamasceno <wemersondamasceno7@gmail.com>
2023-12-27 10:02:59 -0300

    feat: create project colors

---
+++ lib/core/colors/study_flow_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StudyFlowColors {
+  final Color primary = const Color(0xFFF57800);
+  final Color secondary = const Color(0xFF021030);
+
+  final Color successPure = const Color(0xFF55B938);
+  final Color successLight = const Color(0xFF9EE889);
+
+  final Color errorPure = const Color(0xFFD65745);
+  final Color errorLight = const Color(0xFFFF6F5B);
+}
