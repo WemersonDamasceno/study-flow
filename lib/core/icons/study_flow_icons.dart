61f4243fe830627cada93d55e6f0b457030677ea
WemersonDamasceno <wemersondamasceno7@gmail.com>
2023-12-27 10:38:36 -0300

    feat: add class to get assets

---
+++ lib/core/icons/study_flow_icons.dart
@@ -0,0 +1,5 @@
+class StudyFlowIcons {
+  final String animationIcon = 'assets/animations/animation_icon.json';
+  final String animationDontHaveData =
+      'assets/animations/animation_dont_have_data.json';
+}
