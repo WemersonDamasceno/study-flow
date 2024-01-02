c3c2c1a28b12302ce8b845b7c3187c80830fe894
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-02 10:31:22 -0300

    test: create unit test for screen splash

--- test/presentation/splash/view/splash_view_test.dart
+++ test/presentation/splash/view/splash_view_test.dart
@@ -1,48 +1,98 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:lottie/lottie.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:study_flow/core/utils/shared_preferences.dart';
+import 'package:study_flow/core/enums/status_enum.dart';
+import 'package:study_flow/data/datasources/shared_preferences/shared_preferences_datasource.dart';
+import 'package:study_flow/di/di.dart';
+import 'package:study_flow/presentation/splash/bloc/get_user_in_local_storage_bloc.dart';
 import 'package:study_flow/presentation/splash/splash_imports.dart';
 
-class MockSharedPref extends Mock implements SharedPref {}
+class MockSharedPref extends Mock implements SharedPrefDatasourceImpl {}
+
+class MockGetUserInLocalStorageBloc
+    extends MockBloc<GetUserInLocalStorageEvent, GetUserInLocalStorageState>
+    implements GetUserInLocalStorageBloc {}
 
 void main() {
   late MockSharedPref mockSharedPref;
 
-  setUp(() {
+  late Widget child;
+  late GetUserInLocalStorageBloc getUserInLocalStorageBloc;
+
+  setUp(() async {
+    await getIt.reset();
+
+    getUserInLocalStorageBloc = MockGetUserInLocalStorageBloc();
     mockSharedPref = MockSharedPref();
+
+    getIt.registerFactory<GetUserInLocalStorageBloc>(
+        () => getUserInLocalStorageBloc);
+
+    child = BlocProvider<GetUserInLocalStorageBloc>(
+      create: (context) => getUserInLocalStorageBloc,
+      child: const MaterialApp(
+        home: SplashView(),
+      ),
+    );
   });
 
-  testWidgets('SplashView should navigate to the correct page',
+  testWidgets('SplashView should show to the SUCCESS screen',
       (WidgetTester tester) async {
-    // Mock SharedPref to return a non-null value for "name"
+    //Arrange
     when(() => mockSharedPref.read("name")).thenAnswer((_) async => 'John Doe');
+    when(() => getUserInLocalStorageBloc.state).thenReturn(
+      const GetUserInLocalStorageState(
+        status: StatusEnum.success,
+      ),
+    );
 
-    await tester.pumpWidget(const MaterialApp(
-      home: SplashView(),
-    ));
+    // Act
+    await tester.pumpWidget(child);
 
-    // Verify if the initial widgets are rendered
-    expect(find.byType(Scaffold), findsOneWidget);
-    expect(find.byType(SafeArea), findsOneWidget);
-    expect(find.byType(Center), findsOneWidget);
-    expect(find.byType(Column), findsOneWidget);
-    expect(find.byType(SizedBox), findsOneWidget);
-    expect(find.byType(LottieBuilder), findsOneWidget);
+    // Assert
     expect(find.text("Study Flow"), findsOneWidget);
+    expect(
+      find.byKey(const ValueKey("success_screen_widget")),
+      findsOneWidget,
+    );
+  });
+
+  testWidgets('SplashView should show to the ERROR screen',
+      (WidgetTester tester) async {
+    //Arrange
+    when(() => mockSharedPref.read("name")).thenAnswer((_) async => 'John Doe');
+    when(() => getUserInLocalStorageBloc.state).thenReturn(
+      const GetUserInLocalStorageState(
+        status: StatusEnum.error,
+      ),
+    );
+
+    // Act
+    await tester.pumpWidget(child);
+
+    // Assert
+    expect(
+      find.byKey(const ValueKey("error_screen_widget")),
+      findsOneWidget,
+    );
   });
 
   testWidgets('SplashView should navigate to LoginView when name is null',
       (WidgetTester tester) async {
-    // Mock SharedPref to return null for "name"
+    // Arrange
     when(() => mockSharedPref.read("name")).thenAnswer((_) async => null);
+    when(() => getUserInLocalStorageBloc.state).thenReturn(
+      const GetUserInLocalStorageState(
+        status: StatusEnum.empty,
+      ),
+    );
 
-    await tester.pumpWidget(const MaterialApp(
-      home: SplashView(),
-    ));
+    // Act
+    await tester.pumpWidget(child);
 
-    // Verify if the next screen (LoginView) is called correctly
-    expect(find.byType(SplashView), findsOneWidget);
+    // Assert
+    expect(find.byType(SplashView), findsOneWidget); //Alterar para login
   });
 }
