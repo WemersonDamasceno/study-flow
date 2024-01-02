dcef35205cc6611b19ff5802a384e43c20e3e080
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-02 10:27:11 -0300

    feat: create class UserModel

---
+++ lib/data/models/user/user_model.dart
@@ -0,0 +1,37 @@
+import 'package:study_flow/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  const UserModel({
+    required String id,
+    required String name,
+    required String email,
+  }) : super(
+          id: id,
+          name: name,
+          email: email,
+        );
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+    };
+  }
+
+  factory UserModel.fromEntity(UserEntity entity) {
+    return UserModel(
+      id: entity.id,
+      name: entity.name,
+      email: entity.email,
+    );
+  }
+}
