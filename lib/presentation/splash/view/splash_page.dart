31af33a187a8489a9d0d118f41ab90780cf78edd
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-02 10:30:22 -0300

    feat: create Splash screen with Mixin

--- lib/presentation/splash/view/splash_page.dart
+++ lib/presentation/splash/view/splash_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:study_flow/di/di.dart';
+import 'package:study_flow/presentation/splash/bloc/get_user_in_local_storage_bloc.dart';
 import 'package:study_flow/presentation/splash/splash_imports.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const SplashView();
+    return BlocProvider<GetUserInLocalStorageBloc>(
+      create: (_) => getIt<GetUserInLocalStorageBloc>(),
+      child: const SplashView(),
+    );
   }
 }
