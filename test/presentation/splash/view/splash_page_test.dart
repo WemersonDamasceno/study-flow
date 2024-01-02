c6977ebd118d480304bb0597a87930d8f70f988e
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-02 10:31:22 -0300

    test: create unit test for screen splash

--- test/presentation/splash/view/splash_page_test.dart
+++ test/presentation/splash/view/splash_page_test.dart
@@ -1,16 +1,48 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:study_flow/core/enums/status_enum.dart';
+import 'package:study_flow/di/di.dart';
+import 'package:study_flow/presentation/splash/bloc/get_user_in_local_storage_bloc.dart';
 import 'package:study_flow/presentation/splash/splash_imports.dart';
 
+class MockGetUserInLocalStorageBloc
+    extends MockBloc<GetUserInLocalStorageEvent, GetUserInLocalStorageState>
+    implements GetUserInLocalStorageBloc {}
+
 void main() {
+  late Widget child;
+  late GetUserInLocalStorageBloc getUserInLocalStorageBloc;
+
+  setUp(() async {
+    await getIt.reset();
+
+    getUserInLocalStorageBloc = MockGetUserInLocalStorageBloc();
+    getIt.registerFactory<GetUserInLocalStorageBloc>(
+        () => getUserInLocalStorageBloc);
+
+    child = BlocProvider<GetUserInLocalStorageBloc>(
+      create: (context) => getUserInLocalStorageBloc,
+      child: const MaterialApp(
+        home: SplashPage(),
+      ),
+    );
+  });
   testWidgets('SplashPage should render SplashView',
       (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp(
-      home: SplashPage(),
-    ));
+    //Arrange
+    when(() => getUserInLocalStorageBloc.state).thenReturn(
+      const GetUserInLocalStorageState(
+        status: StatusEnum.initial,
+      ),
+    );
 
-    // Verify that SplashView is rendered.
+    // Act
+    await tester.pumpWidget(child);
+
+    // Assert
     expect(find.byType(SplashView), findsOneWidget);
   });
 }
