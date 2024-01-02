b35f122d40075a228bb5ddd7192f6e83977b8644
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-02 10:31:08 -0300

    test: create unit test for bloC

---
+++ test/presentation/splash/bloc/get_user_in_local_storage_event_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:study_flow/presentation/splash/bloc/get_user_in_local_storage_bloc.dart';
+
+void main() {
+  test('GetUserInLocalStorageEvent props', () {
+    const event1 = GetUserInLocalStorage(key: 'test_key');
+    const event2 = GetUserInLocalStorage(key: 'test_key');
+
+    expect(event1, event2);
+    expect(event1.hashCode, event2.hashCode);
+  });
+
+  test('GetUserInLocalStorageEvent props with different key', () {
+    const event1 = GetUserInLocalStorage(key: 'test_key_1');
+    const event2 = GetUserInLocalStorage(key: 'test_key_2');
+
+    expect(event1, isNot(event2));
+  });
+
+  test('GetUserInLocalStorage extends GetUserInLocalStorageEvent', () {
+    const GetUserInLocalStorageEvent event = GetUserInLocalStorage(
+      key: 'test_key',
+    );
+
+    expect(event, isA<GetUserInLocalStorageEvent>());
+  });
+
+  test('GetUserInLocalStorageEvent props', () {
+    const event = GetUserInLocalStorage(key: 'test_key');
+    expect(event.props, isNotEmpty);
+  });
+}
