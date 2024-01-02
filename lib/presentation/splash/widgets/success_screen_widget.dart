6ce596e7913d50ed325754e08fb36befeca82f67
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-02 10:30:45 -0300

    feat: create screen success and screen error

---
+++ lib/presentation/splash/widgets/success_screen_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:study_flow/core/colors/study_flow_colors.dart';
+import 'package:study_flow/core/icons/study_flow_icons.dart';
+
+class SuccessScreenWidget extends StatelessWidget {
+  const SuccessScreenWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: size.width * .5,
+            child: LottieBuilder.asset(
+              StudyFlowIcons().animationIcon,
+            ),
+          ),
+          Text(
+            "Study Flow",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: StudyFlowColors().secondary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
