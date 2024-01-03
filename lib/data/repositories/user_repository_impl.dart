db1cbbcac5f88ce2ea94c61b84e941db8613109b
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-03 16:19:07 -0300

    feat: implement method removeToken and saveToken

--- lib/data/repositories/user_repository_impl.dart
+++ lib/data/repositories/user_repository_impl.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
+import 'package:study_flow/core/enums/shared_pref_keys_enum.dart';
 import 'package:study_flow/core/errors/failure.dart';
 import 'package:study_flow/data/datasources/shared_preferences/shared_preferences_datasource.dart';
-import 'package:study_flow/domain/entities/user_entity.dart';
 import 'package:study_flow/domain/repositories/user/user_repository.dart';
 
 @LazySingleton(as: UserRepository)
@@ -12,22 +12,39 @@ class UserRepositoryImpl implements UserRepository {
   UserRepositoryImpl({required this.userLocalDataSource});
 
   @override
-  Future<Either<Failure, UserEntity?>> getUserInLocalStorage() async {
+  Future<Either<Failure, String?>> getToken() async {
     try {
-      final UserEntity? user = await userLocalDataSource.read("user");
+      final String? user = await userLocalDataSource.getToken(
+        SharedPrefKeysEnum.token,
+      );
       return Future.value(Right(user));
     } on Exception {
-      return const Left(SharedPrefFailure(message: 'Error reading user'));
+      return const Left(SharedPrefFailure(message: 'Error reading token.'));
     }
   }
 
   @override
-  Future<Either<Failure, void>> removeUserInLocalStorage() {
-    throw UnimplementedError();
+  Future<Either<Failure, bool>> removeToken() async {
+    try {
+      await userLocalDataSource.removeToken(
+        SharedPrefKeysEnum.token,
+      );
+      return Future.value(const Right(true));
+    } catch (e) {
+      return const Left(SharedPrefFailure(message: 'Error removing token.'));
+    }
   }
 
   @override
-  Future<Either<Failure, void>> saveUserInLocalStorage(UserEntity user) {
-    throw UnimplementedError();
+  Future<Either<Failure, bool>> saveToken(String token) async {
+    try {
+      await userLocalDataSource.saveToken(
+        SharedPrefKeysEnum.token,
+        token,
+      );
+      return Future.value(const Right(true));
+    } catch (e) {
+      return const Left(SharedPrefFailure(message: 'Error saving token.'));
+    }
   }
 }
