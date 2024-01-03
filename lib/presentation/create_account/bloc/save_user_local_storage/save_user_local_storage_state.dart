c1a1842838b922bd31821eb4c7a514d3e8848b25
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-03 16:31:34 -0300

    chore: run dart-format

--- lib/presentation/create_account/bloc/save_user_local_storage/save_user_local_storage_state.dart
+++ lib/presentation/create_account/bloc/save_user_local_storage/save_user_local_storage_state.dart
@@ -2,7 +2,7 @@ part of 'save_user_local_storage_bloc.dart';
 
 sealed class SaveUserLocalStorageState extends Equatable {
   const SaveUserLocalStorageState();
-  
+
   @override
   List<Object> get props => [];
 }
