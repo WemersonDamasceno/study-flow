4b95d4f18e9fe786501241d99f6c04226d5a30cf
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-03 09:19:37 -0300

    chore: run build_runner

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -10,15 +10,17 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:study_flow/data/datasources/shared_preferences/shared_preferences_datasource.dart'
+import 'package:study_flow/core/widgets/buttons/button_manager_status/button_manager_status_bloc.dart'
     as _i3;
-import 'package:study_flow/data/repositories/user_repository_impl.dart' as _i5;
-import 'package:study_flow/domain/repositories/user/user_repository.dart'
+import 'package:study_flow/data/datasources/shared_preferences/shared_preferences_datasource.dart'
     as _i4;
+import 'package:study_flow/data/repositories/user_repository_impl.dart' as _i6;
+import 'package:study_flow/domain/repositories/user/user_repository.dart'
+    as _i5;
 import 'package:study_flow/domain/usecases/get_user_in_local_storage_usecase.dart'
-    as _i6;
-import 'package:study_flow/presentation/splash/bloc/get_user_in_local_storage_bloc.dart'
     as _i7;
+import 'package:study_flow/presentation/splash/bloc/get_user_in_local_storage_bloc.dart'
+    as _i8;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -31,18 +33,20 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.SharedPreferencesImpl>(() => _i3.SharedPreferencesImpl());
-    gh.factory<_i3.SharedPrefDatasource>(() => _i3.SharedPrefDatasourceImpl(
-        sharedPreferencesImpl: gh<_i3.SharedPreferencesImpl>()));
-    gh.lazySingleton<_i4.UserRepository>(() => _i5.UserRepositoryImpl(
-        userLocalDataSource: gh<_i3.SharedPrefDatasource>()));
-    gh.lazySingleton<_i6.GetUserInLocalStorageUsecase>(() =>
-        _i6.GetUserInLocalStorageUsecase(
-            userRepository: gh<_i4.UserRepository>()));
-    gh.factory<_i7.GetUserInLocalStorageBloc>(() =>
-        _i7.GetUserInLocalStorageBloc(
+    gh.factory<_i3.ButtonManagerStatusBloc>(
+        () => _i3.ButtonManagerStatusBloc());
+    gh.factory<_i4.SharedPreferencesImpl>(() => _i4.SharedPreferencesImpl());
+    gh.factory<_i4.SharedPrefDatasource>(() => _i4.SharedPrefDatasourceImpl(
+        sharedPreferencesImpl: gh<_i4.SharedPreferencesImpl>()));
+    gh.lazySingleton<_i5.UserRepository>(() => _i6.UserRepositoryImpl(
+        userLocalDataSource: gh<_i4.SharedPrefDatasource>()));
+    gh.lazySingleton<_i7.GetUserInLocalStorageUsecase>(() =>
+        _i7.GetUserInLocalStorageUsecase(
+            userRepository: gh<_i5.UserRepository>()));
+    gh.factory<_i8.GetUserInLocalStorageBloc>(() =>
+        _i8.GetUserInLocalStorageBloc(
             getUserInLocalStorageUsecase:
-                gh<_i6.GetUserInLocalStorageUsecase>()));
+                gh<_i7.GetUserInLocalStorageUsecase>()));
     return this;
   }
 }
