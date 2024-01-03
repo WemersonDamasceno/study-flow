1c1ea49ec8927489a4f9af895785d1a3ef39a8fd
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-03 16:24:33 -0300

    test(Repository): create test for method remove and save

--- test/data/repositories/user_repository_impl_test.dart
+++ test/data/repositories/user_repository_impl_test.dart
@@ -1,59 +1,133 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:study_flow/core/enums/shared_pref_keys_enum.dart';
+import 'package:study_flow/core/errors/exceptions.dart';
 import 'package:study_flow/core/errors/failure.dart';
 import 'package:study_flow/data/datasources/shared_preferences/shared_preferences_datasource.dart';
 import 'package:study_flow/data/repositories/user_repository_impl.dart';
-import 'package:study_flow/domain/entities/user_entity.dart';
-import 'package:test/test.dart';
+import 'package:study_flow/domain/repositories/user/user_repository.dart';
 
 class MockSharedPrefDatasource extends Mock implements SharedPrefDatasource {}
 
 void main() {
-  late UserRepositoryImpl userRepository;
-  late MockSharedPrefDatasource mockSharedPrefDatasource;
+  late UserRepository userRepository;
+  late SharedPrefDatasource mockUserLocalDataSource;
+
+  const token = "A3X-42G-M1NDTR1X-789";
 
   setUp(() {
-    mockSharedPrefDatasource = MockSharedPrefDatasource();
+    mockUserLocalDataSource = MockSharedPrefDatasource();
     userRepository =
-        UserRepositoryImpl(userLocalDataSource: mockSharedPrefDatasource);
+        UserRepositoryImpl(userLocalDataSource: mockUserLocalDataSource);
   });
 
-  group('UserRepositoryImpl', () {
-    const String userId = '1';
-    const String userName = 'John Doe';
-    const String userEmail = 'john.doe@example.com';
-
-    const UserEntity userEntity =
-        UserEntity(id: userId, name: userName, email: userEmail);
-
-    test('getUserInLocalStorage - should return user from datasource',
-        () async {
+  group('getToken', () {
+    test('should return user entity from local data source', () async {
       // Arrange
-      when(() => mockSharedPrefDatasource.read(any()))
-          .thenAnswer((_) async => userEntity);
+
+      when(() => mockUserLocalDataSource.getToken(SharedPrefKeysEnum.token))
+          .thenAnswer((_) async => token);
 
       // Act
-      final result = await userRepository.getUserInLocalStorage();
+      final result = await userRepository.getToken();
 
       // Assert
-      expect(result, equals(const Right(userEntity)));
-      verify(() => mockSharedPrefDatasource.read("user")).called(1);
+      expect(result, equals(const Right(token)));
+      verify(() => mockUserLocalDataSource.getToken(SharedPrefKeysEnum.token))
+          .called(1);
+      verifyNoMoreInteractions(mockUserLocalDataSource);
     });
 
-    test(
-        'getUserInLocalStorage - should return SharedPrefFailure if an error occurs',
-        () async {
+    test('should return a failure when an exception occurs', () async {
       // Arrange
-      when(() => mockSharedPrefDatasource.read('user'))
-          .thenThrow(Exception('Error reading user'));
+      when(() => mockUserLocalDataSource.getToken(SharedPrefKeysEnum.token))
+          .thenThrow(Exception());
 
       // Act
-      final result = await userRepository.getUserInLocalStorage();
+      final result = await userRepository.getToken();
 
       // Assert
+
       expect(result.fold((l) => l, (r) => null),
-          const SharedPrefFailure(message: 'Error reading user'));
-      verify(() => mockSharedPrefDatasource.read("user")).called(1);
+          const SharedPrefFailure(message: 'Error reading token.'));
+      verify(() => mockUserLocalDataSource.getToken(SharedPrefKeysEnum.token))
+          .called(1);
+      verifyNoMoreInteractions(mockUserLocalDataSource);
+    });
+  });
+
+  group('removeToken', () {
+    test('should remove token from local data source', () async {
+      // Arrange
+      when(() => mockUserLocalDataSource.removeToken(SharedPrefKeysEnum.token))
+          .thenAnswer((_) async {
+        return true;
+      });
+
+      // Act
+      final result = await userRepository.removeToken();
+
+      // Assert
+      expect(result, equals(const Right(true)));
+      verify(() =>
+              mockUserLocalDataSource.removeToken(SharedPrefKeysEnum.token))
+          .called(1);
+      verifyNoMoreInteractions(mockUserLocalDataSource);
+    });
+
+    test('should return a failure when an exception occurs', () async {
+      // Arrange
+      when(() => mockUserLocalDataSource.removeToken(SharedPrefKeysEnum.token))
+          .thenThrow(
+              const LocalStorageException(message: 'Error removing token.'));
+
+      // Act
+      final result = await userRepository.removeToken();
+
+      // Assert
+
+      expect(result.fold((l) => l, (r) => null),
+          const SharedPrefFailure(message: 'Error removing token.'));
+    });
+  });
+
+  group('saveToken', () {
+    test('should save token to local data source', () async {
+      // Arrange
+      const token = 'sample_token';
+      when(() => mockUserLocalDataSource.saveToken(
+          SharedPrefKeysEnum.token, token)).thenAnswer((_) async {
+        return true;
+      });
+
+      // Act
+      final result = await userRepository.saveToken(token);
+
+      // Assert
+      expect(result, equals(const Right(true)));
+      verify(() => mockUserLocalDataSource.saveToken(
+          SharedPrefKeysEnum.token, token)).called(1);
+      verifyNoMoreInteractions(mockUserLocalDataSource);
+    });
+
+    test('should return a failure when an exception occurs', () async {
+      // Arrange
+      const token = 'sample_token';
+      when(() => mockUserLocalDataSource.saveToken(
+          SharedPrefKeysEnum.token, token)).thenThrow(Exception());
+
+      // Act
+      final result = await userRepository.saveToken(token);
+
+      // Assert
+      expect(
+          result,
+          equals(
+              const Left(SharedPrefFailure(message: 'Error saving token.'))));
+      verify(() => mockUserLocalDataSource.saveToken(
+          SharedPrefKeysEnum.token, token)).called(1);
+      verifyNoMoreInteractions(mockUserLocalDataSource);
     });
   });
 }
