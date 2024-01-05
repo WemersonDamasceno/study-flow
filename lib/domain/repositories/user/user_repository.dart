1f6cfaef9002139d5201fa7ba23a018d9b969bb2
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-05 08:33:30 -0300

    feat: create methods save, get and remove user in repository

--- lib/domain/repositories/user/user_repository.dart
+++ lib/domain/repositories/user/user_repository.dart
@@ -1,8 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:study_flow/core/errors/failure.dart';
+import 'package:study_flow/domain/entities/user_entity.dart';
 
 abstract class UserRepository {
   Future<Either<Failure, String?>> getToken();
   Future<Either<Failure, bool>> saveToken(String token);
   Future<Either<Failure, bool>> removeToken();
+
+  Future<Either<Failure, UserEntity>> saveUser(UserEntity user);
+  Future<Either<Failure, UserEntity>> getUser(String id);
+  Future<Either<Failure, bool>> removeUser(String id);
 }
