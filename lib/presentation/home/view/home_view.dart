e382b8fdf9f44e9f9b276200da94e4b8f8002e32
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-05 08:45:33 -0300

    feat: create base for home screen

---
+++ lib/presentation/home/view/home_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: const Center(
+        child: Text('HomePage'),
+      ),
+    );
+  }
+}
