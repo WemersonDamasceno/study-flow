b7eef02e338b0e74236d88f7ed0a4bd75c39007b
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-05 12:06:22 -0300

    refactor: update CreateAccount

--- lib/presentation/home/view/home_view.dart
+++ lib/presentation/home/view/home_view.dart
@@ -14,18 +14,13 @@ class _HomeViewState extends State<HomeView> {
   }
 
   @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomePage'),
+        title: const Text('Title'),
       ),
       body: const Center(
-        child: Text('HomePage'),
+        child: Text("Home page"),
       ),
     );
   }
