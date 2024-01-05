66b20ac3f83bbe73abd620e54ae008584355d57b
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-05 16:51:40 -0300

    feat: create Home screen

--- lib/presentation/home/view/home_page.dart
+++ lib/presentation/home/view/home_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:study_flow/di/di.dart';
+import 'package:study_flow/presentation/home/bloc/select_tag_priority/select_tag_priority_bloc.dart';
 import 'package:study_flow/presentation/home/view/home_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const HomeView();
+    return BlocProvider(
+      create: (_) => getIt<SelectTagPriorityBloc>(),
+      child: const HomeView(),
+    );
   }
 }
