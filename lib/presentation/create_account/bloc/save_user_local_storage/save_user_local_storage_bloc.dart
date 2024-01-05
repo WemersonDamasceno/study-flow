a48e33de5cbcbcd0d2403a65a16d19884dc158d2
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-01-05 08:38:34 -0300

    feat: create bloC SaveUserLocalStorageBloc

--- lib/presentation/create_account/bloc/save_user_local_storage/save_user_local_storage_bloc.dart
+++ lib/presentation/create_account/bloc/save_user_local_storage/save_user_local_storage_bloc.dart
@@ -1,14 +1,42 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
+import 'package:study_flow/core/enums/status_enum.dart';
+import 'package:study_flow/domain/entities/user_entity.dart';
+import 'package:study_flow/domain/usecases/user/save_user_usecase.dart';
 
 part 'save_user_local_storage_event.dart';
 part 'save_user_local_storage_state.dart';
 
+@injectable
 class SaveUserLocalStorageBloc
     extends Bloc<SaveUserLocalStorageEvent, SaveUserLocalStorageState> {
-  SaveUserLocalStorageBloc() : super(SaveUserLocalStorageInitial()) {
-    on<SaveUserLocalStorageEvent>((event, emit) {
-      // implement event handler
-    });
+  final SaveUserUsecase _saveUserUsecase;
+
+  SaveUserLocalStorageBloc({required SaveUserUsecase saveUserUsecase})
+      : _saveUserUsecase = saveUserUsecase,
+        super(const SaveUserLocalStorageState()) {
+    on<SaveUserLocalStorage>(_saveUser);
+  }
+
+  _saveUser(
+    SaveUserLocalStorage event,
+    Emitter<SaveUserLocalStorageState> emit,
+  ) async {
+    emit(const SaveUserLocalStorageState(status: StatusEnum.loading));
+
+    final failureOrSuccess = await _saveUserUsecase(
+      SaveUserParams(user: event.userEntity),
+    );
+
+    failureOrSuccess.fold(
+      (failure) {
+        emit(const SaveUserLocalStorageState(status: StatusEnum.error));
+      },
+      (success) {
+        emit(const SaveUserLocalStorageState(status: StatusEnum.initial));
+        emit(const SaveUserLocalStorageState(status: StatusEnum.success));
+      },
+    );
   }
 }
